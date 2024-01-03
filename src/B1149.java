4ba8b3877ef9177cba2ae58c5d2327838bed38f8
skwogus98 <skwogus98@gmail.com>
2024-01-04 04:44:04 +0900

    백준 1149 해결

--- src/B1149.java
+++ src/B1149.java
@@ -10,12 +10,12 @@ public class B1149 {
             for (int j = 0; j < 3; j++) {
                 arr[j] = sc.nextInt();
             }
-            //j번째를 선택하는 경우는 이전에 j번째를 선택하지 않았던 경우에 최소값과의 합만 찾으면 된다.
+            //j 번째를 선택하는 경우는 이전에 j 번째를 선택하지 않았던 경우에 최소값과의 합만 찾으면 된다.
             for (int j = 0; j < 3; j++) {
                 dp[i][j] = Math.min(dp[i-1][(j+1)%3],dp[i-1][(j+2)%3])+arr[j];
             }
         }
-        //마지막까지 dp순회한 이후 최소값을 찾는다.
+        //마지막까지 dp 순회한 이후 최소값을 찾는다.
         System.out.println(Math.min(dp[N][0],Math.min(dp[N][1],dp[N][2])));
     }
 }
